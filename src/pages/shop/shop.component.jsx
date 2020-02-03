c08b735c38aeea6f5d6a7757d777213b7e075748
Jado <m.mohamed.jad@gmail.com>
2020-02-02 23:18:51 -0500

    init commit

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import SHOP_DATA from './shop.data';
+import SHOP_DATA from '../shop/shop.data';
 import ColletionPreview from '../../components/collection-preview/collection-preview';
 
 
